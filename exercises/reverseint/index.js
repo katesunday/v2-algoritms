c932e89939841f4d11545e3f60d2e74db95b0766
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-12-04 16:29:29 +0400

    reverse string 2v

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,11 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let stringified = Math.abs(n).toString()
+  let reversed = stringified.split('').reverse().join('')
+  // return n > 0 ? Number(reversed) : -Number(reversed)
+  return Number(reversed) * Math.sign(n)
+}
 
 module.exports = reverseInt

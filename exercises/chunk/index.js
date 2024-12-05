06d80128c20a8939dbfdb0326f5c30bf5f1a058d
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-12-05 17:39:22 +0400

    chunk array

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,15 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let result = []
+  for (let i = 0; i < array.length; i += size) {
+    let cut = array.slice(i, i + size)
+    result.push(cut)
+  }
 
+  return result
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2))
 module.exports = chunk

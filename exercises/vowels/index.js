20601ef4c10a2c80b4cdb263a939199f25d7170b
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-12-10 15:30:16 +0300

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,18 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let vowels = ['a', 'e', 'i', 'o', 'u']
+  let counter = 0
 
+  for (let el of str.toLowerCase()) {
+    if (vowels.includes(el)) {
+      counter++
+    }
+  }
+
+  return counter
+}
+
+console.log(vowels('abcdefghijklmnopqrstuvwxyz'))
 module.exports = vowels

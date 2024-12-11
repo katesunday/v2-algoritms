3845f3181c6ef55cd0691f312601fecc99de2af2
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-12-11 16:36:27 +0300

    fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,12 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  let result = [0, 1];
+  for (let i = 2; i <= n; ++i) {
+    result.push(result[i - 1] + result[i - 2]);
+  }
+  return result[n];
+}
 
 module.exports = fib;

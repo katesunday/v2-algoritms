7d2847b969a6d902f96afbabaacdd7007a11211f
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-12-15 14:50:59 +0400

    stack

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,25 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+  push(data) {
+    this.stack.push(data);
+  }
+  pop() {
+    return this.stack.pop();
+  }
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+}
 
+const list = new Stack();
+list.push(1);
+list.push(2);
+list.push(3);
+list.pop();
+console.log(list);
 module.exports = Stack;

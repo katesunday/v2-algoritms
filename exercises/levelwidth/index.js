a2a153c94459431a7727d3c80af5f279f862f969
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-12-18 17:14:35 +0400

    tree  width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,30 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+const Node = require('./node');
+
+function levelWidth(root) {
+  let arr = [root, null];
+  let counters = [0];
+  while (arr.length > 1) {
+    let node = arr.shift();
+    if (node === null) {
+      counters.push(0);
+      arr.push(null);
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+}
+
+const root = new Node(0);
+root.add(1);
+root.add(2);
+root.add(3);
+root.children[0].add(4);
+root.children[2].add(5);
+console.log(levelWidth(root));
 
 module.exports = levelWidth;

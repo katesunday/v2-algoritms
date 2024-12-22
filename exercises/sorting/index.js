d09ba91f6bdab00c0909b8fa8a5c9925ab6c8ce6
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-12-22 14:17:32 +0400

    selection sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,7 +16,22 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (lowest !== i) {
+      let lesser = arr[lowest];
+      arr[lowest] = arr[i];
+      arr[i] = lesser;
+    }
+  }
+  return arr;
+}
 
 function mergeSort(arr) {}
 

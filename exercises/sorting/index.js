c34c3c1bc2f7238140f42600676b5d82aa66d7f1
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-12-22 14:39:02 +0400

    merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -33,8 +33,27 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  let center = Math.floor(arr.length / 2);
+  let left = arr.slice(0, center);
+  let right = arr.slice(center);
 
-function merge(left, right) {}
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  return [...result, ...left, ...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };

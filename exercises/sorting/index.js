3f3fd51ba5734f0088a2cbdf4f05d586807749de
Ekaterina Kopylova <ekaterina.sunday@gmail.com>
2024-12-22 14:04:22 +0400

    bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,7 +1,20 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
-function bubbleSort(arr) {}
+function bubbleSort(arr) {
+  // return arr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let first = arr[j];
+        let second = arr[j + 1];
+        arr[j] = second;
+        arr[j + 1] = first;
+      }
+    }
+  }
+  return arr;
+}
 
 function selectionSort(arr) {}
 
